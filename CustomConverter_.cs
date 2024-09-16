f64534311c84cb5d7a0b89f78db749bd0775462c
BatischevAM <batischevam@mer.ci.nsu.ru>
2024-09-16 09:59:36 +0700

    Add files via upload

---
+++ CustomConverter_.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConverter
+{
+    public class CustomConverter_
+    {
+        public void Convert(string input, out int result)
+        {
+            int.TryParse(input, out result);
+        }
+        public void Convert(string input, out double result)
+        {
+            double.TryParse(input, out result);
+        }
+        public void Convert(string input, out bool result)
+        {
+            bool.TryParse(input, out result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using CustomConverter;
+class Program
+{
+    static void Main()
+    {
+        CustomConverter_ converter = new CustomConverter_();
+
+        string IntInput = "123";
+        converter.Convert(IntInput, out int IntResult);
+        Console.WriteLine($"Convert '{IntInput}' in int: {IntResult}");
+
+        string DoubleInput = "123,45";
+        converter.Convert(DoubleInput, out double DoubleResult);
+        Console.WriteLine($"Convert '{DoubleInput}' in double: {DoubleResult}");
+
+        string BoolInput = "true";
+        converter.Convert(BoolInput, out bool BoolResult);
+        Console.WriteLine($"Convert '{BoolInput}' in bool: {BoolResult}");
+        Console.ReadKey();
+    }
+}
+
